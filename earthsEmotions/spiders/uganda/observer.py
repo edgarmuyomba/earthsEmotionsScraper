92bfe975f29340b9f6feab90598e2414bb81e2fd
Edgar Muyomba <edgarmatthew>
2024-09-13 13:45:12 +0300

    observer works and article item

--- earthsEmotions/spiders/uganda/observer.py
+++ earthsEmotions/spiders/uganda/observer.py
@@ -2,34 +2,40 @@ from scrapy.spiders import CrawlSpider
 from scrapy.spiders import Rule
 from scrapy.linkextractors import LinkExtractor
 
-
-class OserverSpider(CrawlSpider):
+class ObserverSpider(CrawlSpider):
     name = "observer"
     allowed_domains = ["observer.ug"]
     start_urls = ["https://www.observer.ug"]
     rules = [
-        Rule(LinkExtractor(allow=r"/index.php/[a-z]$"), follow=True),
+        Rule(LinkExtractor(allow=r'/index\.php/[a-z-]+$'), follow=True),
         Rule(LinkExtractor(
-            allow=r"/index.php/[a-z]/\d+\-.*"), callback="parse_items", follow=True)
+            allow=r'/index\.php/[a-z-]+/\d+-[a-z0-9-]+$'), callback='parse_items', follow=True)
     ]
 
     custom_settings = {
-        'User_Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5)'
+        'USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5)'
         'AppleWebKit 537.36 (KHTML, like Gecko) Chrome',
         'DOWNLOAD_DELAY': 2,
         'DOWNLOAD_TIMEOUT': 10,
         'CONCURRENT_REQUESTS': 10,
-        'LOG_LEVEL': 'DEBUG',
+        'LOG_LEVEL': 'INFO',
         'LOG_FILE': 'observer.log',
         'FEED_URI': 'Outputs/observer.json',
         'FEED_FORMAT': 'json'
     }
 
     def parse_items(self, response):
+        self.logger.debug(f"Parsing page:: {response.url}")
         title = response.css("h1[itemprop='name']::text").get()
+        author = response.css("span[itemprop='name']::text").get()
         date = response.css("time[itemprop='datePublished']::text").get()
+        body = response.css("span[itemprop='articleBody']").get()
 
-        yield {
-            "title": title,
-            "date": date
-        }
+        if title and date:
+            return {
+                "title": title,
+                "author": author,
+                "date": date
+            }
+        else:
+            self.logger.debug(f"No article found on the page:: {response.url}")
